37e3b4a2d1d72b53765025e387d42141c1cefefa
root <mike@lcd-outlet.com>
2017-04-05 13:18:24 -0400

    json2postgres cleaner syntax

--- json2postgres.sh
+++ json2postgres.sh
@@ -1,4 +1,10 @@
-while read -r line;
-	do  postgres "insert into github_test values ('$line')"; 
-	done < <(cat "$1"|tr -d "'" |grep -P "^{\"id.*?}}$")
+#/* while read f;do file_name=$(echo $f|sed -re 's/^\.\///g');bash json2postgres.sh $file_name;done <xaa */
+
+source /media/data/Github/user_defined_functions
+while read -r line;do
+	date=$(echo $1|tr -d "./json");
+	echo $date
+	clean_json=$(echo $line|sed -re 's/\//\/\//g')
+	postgres "insert into github_archive values ('$date','$clean_json') "	
+	done< <(cat "$1"|tr -d "'" |grep -P "^{\"id.*?}}$")
 
