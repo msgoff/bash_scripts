c0aac6dc9224e3221bf8e258ec8ad688ff91e78e
PelicanHPC user <mike@lcd-outlet.com>
2017-05-13 06:01:05 +0000

    working on installers

--- pelican_installer.sh
+++ pelican_installer.sh
@@ -3,18 +3,18 @@ sudo apt-get install vim
 sudo apt-get install python-pip
 sudo apt-get install screen
 sudo apt-get install git
+sudo apt-get install w3m
 sudo pip install -U pip
 sudo pip install jupyter pandas tqdm requests selenium 
 git clone git@github.com:junegunn/fzf
 cd fzf
 ./install
-cd ../
+cd ../ && source ~/.bashrc
 
 
 
 sudo apt install nfs-common
 sudo apt-get install postgresql postgresql-contrib
-pip install psycopg2
 sudo pip install psycopg2
 sudo pip install tqdm
 sudo apt install ncurses-devel
@@ -26,10 +26,7 @@ sudo apt install codesearch
 sudo pip install requests
 sudo apt install pdfgrep
 sudo apt install xpdf
-sudo apt install pdf2htmlex
 sudo apt install parallel
 
-source ~/.bashrc
-xzs(){ fzf --print-query -m --bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all; }
-#make an installer script from history
-#history|xzs|sort -nk 1|grep -oP "[a-z].*" 
+
+
