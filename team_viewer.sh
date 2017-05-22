c629e9d939c2b40120470e02216df55e7766eb83
PelicanHPC user <mike@lcd-outlet.com>
2017-05-22 00:11:13 +0000

    team_viewer

---
+++ team_viewer.sh
@@ -0,0 +1,24 @@
+team_viewer=teamviewer_i386.deb
+
+tv_error()
+{
+
+
+			sudo apt -f install
+			sudo apt update
+			sudo dpkg -i teamviewer_i386.deb
+
+}
+
+
+if [ ! -f "$team_viewer" ];
+		then
+			wget https://download.teamviewer.com/download/teamviewer_i386.deb
+			sudo dpkg --add-architecture i386
+			sudo dpkg -i teamviewer_i386.deb && echo "ok" || tv_error
+
+
+fi
+
+
+tv_error
