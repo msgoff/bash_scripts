bb302951902b2ac65273b0598dfecc8e600e1d0e
Michael Goff <msgoff@users.noreply.github.com>
2017-06-08 20:51:25 +0000

    Create sort_by_filetype.sh

---
+++ sort_by_filetype.sh
@@ -0,0 +1,3 @@
+parallel echo ::: $(find . -type f |rev |cut -d "." -f1|rev|sort |uniq -c |sort -g|awk '$1 > 1'|awk '{print $2}') > file_types
+
+while read f;do mkdir $f; find . -type f -name "*.$f" -exec cp "{}" $f/ \; ;done < <(cat file_types)
