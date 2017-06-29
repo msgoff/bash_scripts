fbc47247508ec1b61770580575b1fa6e17302692
kjh <mike@lcd-outlet.com>
2017-06-29 00:03:24 -0400

    some updates package installer

--- pelican_installer.sh
+++ pelican_installer.sh
@@ -1,20 +1,16 @@
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get install neovim
 sudo apt-get install python-pip
+sudo pip install -U pip
 sudo apt-get install screen
 sudo apt-get install git
 sudo apt-get install w3m
 sudo pip install -U pip
 sudo pip install jupyter pandas tqdm requests selenium 
 wget http://www.scootersoftware.com/bcompare-4.2.1.22354_amd64.deb
-
 git clone git@github.com:junegunn/fzf
 cd fzf
 ./install
-cd ../ && source ~/.bashrc
-
-
-
 sudo apt install nfs-common
 sudo apt-get install postgresql postgresql-contrib
 sudo pip install psycopg2
@@ -26,7 +22,38 @@ sudo apt install golang-go
 sudo apt install silversearcher-ag
 sudo apt install codesearch
 sudo pip install requests
-
-
-ssh-keygen 
-firefox github.com
+sudo apt install bison m4 flex
+sudo apt-get update
+sudo apt-get install screen git -y
+sudo pip install requests[security] pandas virtualenv jupyter pyparsing pytz pyzmq six tornado jsonschema
+sudo apt-get install libcurl3-dev python-pip build-essential python-dev python-setuptools python-numpy -y
+sudo pip install requests[security] pandas virtualenv jupyter pyparsing pytz pyzmq six tornado jsonschema
+sudo pip install selenium sklearn nltk certifi scikit-learn tqdm natsort xlwt xmltodict
+sudo pip install whitenoise lxml django redis
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install build-essential -y
+sudo apt-get install cmake -y
+sudo apt-get install flex bison -y
+sudo apt-get install g++ gfortran -y
+sudo apt-get install git -y
+sudo apt-get install libcurl3-dev -y
+sudo apt-get install libtool automake -y
+sudo apt-get install libtool-bin -y
+sudo apt-get install python-pip python-dev -y
+sudo apt-get remove unity-webapps-common  -y
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install lynis -y
+sudo apt-get install python-pip -y
+sudo apt-get install vim -y
+sudo pip install asciitree
+sudo pip install redis
+sudo pip install tqdm
+sudo pip install --upgrade autopep8
+sudo pip install --upgrade pip
+sudo pip install selenium
+sudo pip install html5lib
+sudo pip install BeautifulSoup4
+sudo apt-get install nodejs-legacy npm ipython ipython-notebook libzmq3-dev
+sudo npm install -g ijavascript
