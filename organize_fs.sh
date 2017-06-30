4e22b204f65ccb2740eb9be4947a2a7016ad8767
Michael Goff <msgoff@users.noreply.github.com>
2017-06-30 22:35:03 +0000

    Update organize_fs.sh

--- organize_fs.sh
+++ organize_fs.sh
@@ -1,2 +1,4 @@
 mkdir ~/Desktop/"$1"
 mlocate .|grep -i "\.$1$" |xargs -I {} cp {} ~/Desktop/"$1"
+#for the top 100 most common files on the system
+#mlocate .|rev |cut -d "." -f1 |rev|sort|uniq -c|sort -g |sed -re 's/^\s+//g'| awk '{print $2}'|tail -n 100|xargs -I {} ./organize_fs.sh {} 
