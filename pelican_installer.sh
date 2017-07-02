688307a443ac2eddcc30a50dfd0966f718b6624c
PelicanHPC user <mike@lcd-outlet.com>
2017-07-02 21:14:22 +0000

    added parallel to intsall script

--- pelican_installer.sh
+++ pelican_installer.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
+sudo apt install parallel
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 sudo apt-get install neovim
 sudo apt-get install python-pip
@@ -6,12 +7,7 @@ sudo pip install -U pip
 sudo apt-get install screen
 sudo apt-get install git
 sudo apt-get install w3m
-sudo pip install -U pip
 sudo pip install jupyter pandas tqdm requests selenium 
-wget http://www.scootersoftware.com/bcompare-4.2.1.22354_amd64.deb
-git clone git@github.com:junegunn/fzf
-cd fzf
-./install
 sudo apt install nfs-common
 sudo apt-get install postgresql postgresql-contrib
 sudo pip install psycopg2
@@ -24,7 +20,6 @@ sudo apt install silversearcher-ag
 sudo apt install codesearch
 sudo pip install requests
 sudo apt install bison m4 flex
-sudo apt-get update
 sudo apt-get install screen git -y
 sudo pip install requests[security] pandas virtualenv jupyter pyparsing pytz pyzmq six tornado jsonschema
 sudo apt-get install libcurl3-dev python-pip build-essential python-dev python-setuptools python-numpy -y
@@ -35,13 +30,11 @@ sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install build-essential -y
 sudo apt-get install cmake -y
-sudo apt-get install flex bison -y
 sudo apt-get install g++ gfortran -y
 sudo apt-get install git -y
 sudo apt-get install libcurl3-dev -y
 sudo apt-get install libtool automake -y
 sudo apt-get install libtool-bin -y
-sudo apt-get install python-pip python-dev -y
 sudo apt-get remove unity-webapps-common  -y
 sudo apt-get update -y
 sudo apt-get install git -y
@@ -56,5 +49,9 @@ sudo pip install --upgrade pip
 sudo pip install selenium
 sudo pip install html5lib
 sudo pip install BeautifulSoup4
+
+wget http://www.scootersoftware.com/bcompare-4.2.1.22354_amd64.deb
+git clone git@github.com:junegunn/fzf
 sudo apt-get install nodejs-legacy npm ipython ipython-notebook libzmq3-dev
+
 sudo npm install -g ijavascript
