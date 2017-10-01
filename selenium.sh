483ccaf898785d3997cc6c353f6ac7b7605c5b1a
mike <mike@lcd-outlet.com>
2017-10-01 01:56:26 +0000

    selenium

---
+++ selenium.sh
@@ -0,0 +1,4 @@
+wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz
+
+tar -vxzf geckodriver-v0.19.0-linux64.tar.gz
+sudo cp geckodriver /usr/bin/
