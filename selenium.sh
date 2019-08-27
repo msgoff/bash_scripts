72585134c61309acae6c34f25b62865ebf397a04
mike <mike@lcd-outlet.com>
2019-08-27 01:47:05 +0000

    remove unused

--- selenium.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/mozilla/geckodriver/releases/download/v0.19.0/geckodriver-v0.19.0-linux64.tar.gz
-
-tar -vxzf geckodriver-v0.19.0-linux64.tar.gz
-sudo cp geckodriver /usr/bin/
--- set_vars.sh
+++
@@ -1,2 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/My Project-d8fd67d81055.json"
-
