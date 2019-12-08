3c40d21d11de0bcc4ba3c2945e631c2b2c218569
Michael Goff <msgoff@users.noreply.github.com>
2019-12-07 23:46:36 -0500

    Delete install_gitlab.sh

--- install_gitlab.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install curl openssh-server ca-certificates postfix -y
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-sudo apt-get install gitlab-ce
-sudo gitlab-ctl reconfigure
