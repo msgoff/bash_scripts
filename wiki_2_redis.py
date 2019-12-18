54a5bec8a2f77a829a572a424e4dc031191abe35
mike <mike@lcd-outlet.com>
2019-12-18 18:25:07 +0000

    clean up files

--- wiki_2_redis.py
+++ wiki_2_redis.py
@@ -1,36 +1,7 @@
 import re
-from redis import Redis
 
-
-
-r = Redis()
 f=open('enwiki-latest-pages-articles-multistream.xml')
 data=f.read()
 
 
 resp=re.findall('<page>.*?</page>',data,re.DOTALL)
-
-
-#becuase it's fun
-f = lambda key, id: {
-    re.sub('>|<', '', key): \
-        str(re.findall('{}(.*?){}'.format(key, key.replace('<', '</')), resp[id], re.DOTALL))}
-
-
-def get_keys(id):
-    return re.findall('<[a-zA-Z]+>', resp[id])
-
-
-def dct_2_redis(id):
-    dct = {}
-    keys = get_keys(id)
-    for key in keys:
-        if 'page' not in key:
-            dct.update(f(key, id))
-
-    r.hmset(id, dct)
-
-
-#load wiki into redis
-gnr8tr = (dct_2_redis(id) for id in range(len(resp)))
-while True: gnr8tr.next()
