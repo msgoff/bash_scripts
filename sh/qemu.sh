fc8181e9fdfdd3436dcbfa820a50f27b6495528e
mike <mike@mathrules.us>
2021-01-04 15:43:50 +0000

    qemu

--- sh/qemu.sh
+++ sh/qemu.sh
@@ -1,6 +1,9 @@
 sudo qemu-system-x86_64 \
-	-m 2000M \
-	-boot d  -cdrom /mnt/ramdisk/acronis.iso \
-	-drive format=raw,file=/dev/sda \
-	-drive format=raw,file=/dev/sdb \
+	-m 8G \
+	-boot d  -cdrom "$1" \
+	-hda "$2" \
+	#-drive format=raw,file=/dev/sda \
+	#-drive format=raw,file=/dev/sdb \
+	-net nic,model=virtio \
+	-net user,hostfwd=tcp::2222-:22
 	-enable-kvm
