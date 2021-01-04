fb5b7846bd0a92924340070c656e9e25bee535fe
mike <mike@mathrules.us>
2021-01-04 22:30:41 +0000

    list python imports

--- Parsing_Python/python_ast/import_lister.py
+++ Parsing_Python/python_ast/import_lister.py
@@ -1,16 +1,41 @@
-source = "import re\nimport ast"
+
+from sys import argv
+import re
 import ast
+f = open(argv[1])
+checksum_file = f.readlines()
+f.close()
+checksum_file =[x.replace('\n','') for x in checksum_file]
+checksum_dict ={filepath:cs for cs,filepath in \
+    [re.split('\s{2,}',x) for x in checksum_file]  
+}
 class ImportLister(ast.NodeVisitor):
     def __init__(self):
-        pass
+        self.imports = {}
+    
     def node_information(self, node, node_type,node_name):
         response = (node.lineno, node.col_offset, node_type, node_name)
         return response
     
     def visit_Import(self,node):
-        print([x.__dict__ for x in node.names],node.lineno,node.col_offset,node.end_lineno,node.end_col_offset)
+        self.imports[(node.lineno,node.col_offset,node.end_lineno,node.end_col_offset)] = [x.__dict__ for x in node.names]
         self.generic_visit(node)
         
-tree = ast.parse(source)
-imports = ImportLister().visit(tree)
-print(imports)
+from pudb import set_trace
+#set_trace()
+
+
+for filepath,cs in checksum_dict.items():
+    try:
+        f = open(filepath)
+        source = f.read()
+        f.close()
+    except:
+        pass
+    try:
+        tree = ast.parse(source)
+        X = ImportLister()
+        X.visit(tree)
+        print({(cs,filepath):X.imports})
+    except:
+        pass
--- sh/qemu.sh
+++ sh/qemu.sh
@@ -2,9 +2,6 @@ sudo qemu-system-x86_64 \
 	-m 8G \
 	-hda "$1" \
 	-smp 4 \
-	-cpu host \
-	#-drive format=raw,file=/dev/sda \
-	#-drive format=raw,file=/dev/sdb \
 	-net nic,model=virtio \
 	-net user,hostfwd=tcp::2222-:22
-	-enable-kvm
+	--enable-kvm
